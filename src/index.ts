9969c280b3b6798c6b995d8aa3b955994d64e251
irohitb <irohitbhatia@gmail.com>
2024-08-12 02:07:32 +0530

    refactor: yargs to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-import { initializeCliCommand } from './cli-commands'
+import yargs from 'yargs';
 
-initializeCliCommand()
+yargs
+    .usage("Usage: $0 <command> [options]")
+    .example(
+        "$0 watch --path {sql_directory}/ --db postgres://postgres:postgres@localhost:5432/postgres",
+        "Watches the SQL path in the given directory and runs modified changes on save directly against the database."
+    )
+    .option("path", {
+        alias: "p",
+        describe: "The path/folder containing SQL files",
+        type: 'string',
+        default: ".",
+    })
+    .command({
+        command: "watch",
+        aliases: ["w"],
+        describe: "Watch and apply changes in SQL files specified in the path flag",
+        builder: (yargs) => {
+            return yargs.option('db', {
+                describe: "URL to access PostgreSQL database",
+                type: "string",
+                demandOption: true,
+            });
+        },
+        handler: (argv) => {
+            const { path, db } = argv;
+            console.log(`Watching SQL files in ${path} and applying changes to ${db}`);
+        },
+    })
+    .demandCommand()
+    .parse();
